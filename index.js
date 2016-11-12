c104c74546c45bb15c67a37de340bbb800519c61
Kevin Fung <kevinfung87@gmail.com>
2016-11-12 13:47:23 +0100

    updated the create message

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express'),
+      morgan = require('morgan'),
+      bodyParser = require('body-parser'),
+      pug = require('pug'),
+      Sequelize = require('sequelize');
+
+var app = express();
+    sequelize = new Sequelize('Kevin', 'Kevin', '', { dialect: 'postgres' });
+
+
+
+app.use(express.static('public'));
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('view engine', 'pug');
+
+var Bulletin = sequelize.define('bulletinboard', {
+  title: Sequelize.STRING,
+  body: Sequelize.STRING
+});
+
+
+app.get('/', (request, response) => {
+  Bulletin.findAll({ order:'id ASC'}).then((messages) => {
+    response.render('messages/index', { messages: messages });
+  });
+});
+
+app.get('/messages/new', (request, response) => {
+  response.render('messages/new');
+});
+
+
+
+app.post('/messages', (request, response) => {
+  console.log('post request komt binnen');
+  if (request.body.title) {
+    Bulletin.create(request.body).then(() => {
+      response.redirect('/');
+    });
+  } else {
+    response.redirect('/messages/new');
+  }
+});
+
+
+
+
+
+sequelize.sync().then(() => {
+  app.listen(3000, () => {
+    console.log('Web Server is running on port 3000');
+  });
+});
